06bf3d2647b81c7d2e77e1bf98dc0afe7c2899e8
athulSreenivas <athulsreenivas1999@gmail.com>
2024-07-03 14:15:41 +0530

    Adding the build

---
+++ build/assets/About-5Z_BSRCS.js
@@ -0,0 +1,20 @@
+import{j as e,M as n,L as a}from"./index-C6cgQBGM.js";import{M as o}from"./index.modern-BeNO9YFM.js";const l=()=>{const i=`
+
+# About Me
+Hi, I'm Athul Sreenivas, a software engineer specialized in web development. With over 3 years of experience, I've honed my skills in PHP, CodeIgniter, MySQL, Python, and JavaScript. I enjoy building efficient, scalable, and user-friendly web applications that solve real-world problems.
+
+# Projects & Achievements
+- Developed and maintained multiple web applications that improved operational efficiency and user engagement.
+- Successfully led projects from conception to deployment, working closely with cross-functional teams to deliver high-quality solutions on time.
+- Implemented security best practices to protect user data and ensure compliance with industry standards.
+
+# I Like
+
+- *Football:* I enjoy playing and watching football, appreciating the strategy and teamwork involved in the game.
+- *Movies:* I'm a movie enthusiast who loves exploring different genres and cinematic styles.
+- *Songs:* Music is a big part of my life, and I enjoy listening to a wide variety of songs.
+- *Swimming:* Swimming is one of my favorite activities, helping me stay fit and relaxed.
+- *Photography:* I have a passion for photography, capturing moments and creating visual stories.
+- *Coding*: I'm passionate about coding
+
+`;return e.jsx(n,{title:"About",description:"Learn about Athul Sreenivas",children:e.jsxs("article",{className:"post markdown",id:"about",children:[e.jsx("header",{children:e.jsxs("div",{className:"title",children:[e.jsx("h2",{children:e.jsx(a,{to:"/about",children:"About Me"})}),e.jsxs("p",{children:["(in about"," ",i.split(/\s+/).filter(t=>t).length," ","words)"]})]})}),e.jsx(o,{children:i})]})})};export{l as default};
